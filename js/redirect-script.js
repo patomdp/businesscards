7d6c16a662b0fe1a011a8372f33c7589783f8cbd
pgmariano <patomdp00@gmail.com>
2022-09-14 12:59:16 +0200

    added bio link instead of Blippar project

--- js/redirect-script.js
+++ js/redirect-script.js
@@ -1,2 +1,4 @@
 if ((window.location.href || "").includes("netlify.app"))
-  window.location.href = "https://ar.blippar.com/oan159vnv8jxt1t53i31d6jk52xxwl10w1wjrlmj";
+  window.location.href = "https://pmariano.bio.link";
+
+  //BlippAr AR project: https://ar.blippar.com/oan159vnv8jxt1t53i31d6jk52xxwl10w1wjrlmj
